bce1a8d825b2323b555605a330839a0619dea7fc
reid-fu <hp main@hp>
2015-10-10 10:14:02 -0400

    updated AlchemyAccessor

--- RedditAssholeDaily/src/AlchemyAccessor.java
+++ RedditAssholeDaily/src/AlchemyAccessor.java
@@ -1,4 +1,23 @@
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import components.xmltree.XMLTree1;
 
 public class AlchemyAccessor {
-
+	private static String key = "a7237fba2dc27fb0a285626142515aa4f87e0bf2";
+	public AlchemyAccessor(){
+		
+	}
+	public String getXML(String comment){
+		try {
+			return new XMLTree1(this.getURL(comment)).toString();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	private String getURL(String comment) throws UnsupportedEncodingException{
+		return "http://gateway-a.watsonplatform.net/calls/"
+				+ "text/TextGetTextSentiment?apikey=" + key + "&text="
+	            + URLEncoder.encode(comment, "UTF-8");
+	}
 }
