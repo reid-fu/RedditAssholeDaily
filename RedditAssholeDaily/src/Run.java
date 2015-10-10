787206a1e8d7c71308330436cfe8d948565b3896
reid-fu <hp main@hp>
2015-10-10 13:58:52 -0400

    added main class

---
+++ RedditAssholeDaily/src/Run.java
@@ -0,0 +1,9 @@
+import reddit.RedditParser;
+
+public class Run {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		//TODO put in IO manager
+		RedditParser parser = new RedditParser(null);
+	}
+}
