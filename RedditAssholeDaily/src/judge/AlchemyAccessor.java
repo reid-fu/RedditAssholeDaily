afb251cd8bc7308716d61f8ca5cba9e5ae60331a
reid-fu <hp main@hp>
2015-10-10 10:42:44 -0400

    added CommentJudge interface

--- RedditAssholeDaily/src/judge/AlchemyAccessor.java
+++ RedditAssholeDaily/src/judge/AlchemyAccessor.java
@@ -5,9 +5,6 @@ import components.xmltree.XMLTree1;
 
 public class AlchemyAccessor {
 	private static String key = "a7237fba2dc27fb0a285626142515aa4f87e0bf2";
-	public AlchemyAccessor(){
-		
-	}
 	public String getXML(String comment){
 		try {
 			return new XMLTree1(this.getURL(comment)).toString();
---
+++ RedditAssholeDaily/src/judge/CommentJudge.java
@@ -0,0 +1,8 @@
+package judge;
+
+public interface CommentJudge {
+	/** @param comment comment to judge
+	 * @return score for comment
+	 */
+	public int score(String comment);
+}
