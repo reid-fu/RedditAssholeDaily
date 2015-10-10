fe8d923ca49f7ad3f1c438fb7c7149d208662906
reid-fu <hp main@hp>
2015-10-10 17:50:21 -0400

    finished implementation of AlchemyJudge

--- RedditAssholeDaily/src/judge/AlchemyJudge.java
+++ RedditAssholeDaily/src/judge/AlchemyJudge.java
@@ -3,14 +3,21 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import components.xmltree.*;
 
+/** judges comments using Alchemy API;
+ * cumulative score calculated using exponential averaging
+ * @author Reid Fu
+ */
 public class AlchemyJudge implements CommentJudge {
 	private static String key = "a7237fba2dc27fb0a285626142515aa4f87e0bf2";
+	private double cumScore = 0;
 	@Override
 	public double score(String comment) {
 		try {
 			XMLTree docSentiment = child(getXMLTree(getURL(comment)), "docSentiment");
-			XMLTree score = child(docSentiment, "score");
-			return Double.parseDouble(score.child(0).label());
+			XMLTree scoreTag = child(docSentiment, "score");
+			double score = Double.parseDouble(scoreTag.child(0).label());
+			this.updateCumScore(score);
+			return score;
 		} catch (UnsupportedEncodingException e) {
 			System.err.println("UTF-8 encoding not supported");
 			return -1;
@@ -18,13 +25,15 @@ public class AlchemyJudge implements CommentJudge {
 	}
 	@Override
 	public double cumulativeScore() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.cumScore;
 	}
 	@Override
 	public Verdict verdict() {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.cumScore < 0){
+			return Verdict.ASSHOLE;
+		} else {
+			return Verdict.NONASSHOLE;
+		}
 	}
 	//PRIVATE METHODS
 	private String getURL(String comment) throws UnsupportedEncodingException{
@@ -42,4 +51,8 @@ public class AlchemyJudge implements CommentJudge {
                 index = i;
         return xml.child(index);
     }
+	private void updateCumScore(double score){
+		double alpha = .5;
+		this.cumScore = alpha*score + (1 - alpha)*this.cumScore;
+	}
 }
