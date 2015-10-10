c1df4d30e78fb2abbb70e6e88c2623eb9d41b92d
reid-fu <hp main@hp>
2015-10-10 10:58:54 -0400

    changed method

--- RedditAssholeDaily/src/judge/AlchemyAccessor.java
+++ RedditAssholeDaily/src/judge/AlchemyAccessor.java
@@ -1,7 +1,7 @@
 package judge;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import components.xmltree.XMLTree1;
+import components.xmltree.*;
 
 public class AlchemyAccessor implements CommentJudge {
 	private static String key = "a7237fba2dc27fb0a285626142515aa4f87e0bf2";
@@ -9,22 +9,6 @@ public class AlchemyAccessor implements CommentJudge {
 	public double score(String comment) {
 		return -1;
 	}
-	private static XMLTree getChildElement(XMLTree xml, String tag) {
-        assert xml != null : "Violation of: xml is not null";
-        assert tag != null : "Violation of: tag is not null";
-        assert xml.isTag() : "Violation of: the label root of xml is a tag";
-        int index = -1;
-        /*
-         * Loops from last to first child and compares the root of that tree to
-         * tag. Returns the lowest index whose root label matches the tag.
-         */
-        for (int i = xml.numberOfChildren() - 1; i >= 0; i--) {
-            if (tag.equals(xml.child(i).label())) {
-                index = i;
-            }
-        }
-        return xml.child(index);
-    }
 	//TEST METHODS
 	public String getXML(String comment){
 		try {
@@ -40,4 +24,11 @@ public class AlchemyAccessor implements CommentJudge {
 				+ "text/TextGetTextSentiment?apikey=" + key + "&text="
 	            + URLEncoder.encode(comment, "UTF-8");
 	}
+	private XMLTree child(XMLTree xml, String tag) {
+        int index = -1;
+        for (int i = xml.numberOfChildren() - 1; i >= 0; i--)
+            if (tag.equals(xml.child(i).label()))
+                index = i;
+        return xml.child(index);
+    }
 }
