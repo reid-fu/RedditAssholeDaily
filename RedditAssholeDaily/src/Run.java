843bd6d6d0aa85028f7edb5bf51e46b6e48651c8
reid-fu <hp main@hp>
2015-10-10 17:56:14 -0400

    prototyped main class

--- RedditAssholeDaily/src/Run.java
+++ RedditAssholeDaily/src/Run.java
@@ -1,17 +1,26 @@
+import javax.swing.JOptionPane;
+
 import io.*;
+import judge.*;
+import judge.CommentJudge.Verdict;
 import reddit.RedditParser;
 
 public class Run {
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		IOManager io = new JIOManager();
 		String username = io.inputUser();
 		if(username == null)
 			System.exit(0);
 		RedditParser parser = new RedditParser(username);
+		CommentJudge judge = new AlchemyJudge();
 		String comment = null;
 		while((comment = parser.nextComment()) != null){
-			
+			judge.score(comment);
+		}
+		if(judge.verdict() == Verdict.ASSHOLE){
+			JOptionPane.showMessageDialog(null, "You're an Asshole");
+		} else {
+			JOptionPane.showMessageDialog(null, "You're not an Asshole");
 		}
 	}
 }
