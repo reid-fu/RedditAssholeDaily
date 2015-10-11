dc4a3b8b9efd53d5383a46bf21327f473e4916a1
reid-fu <hp main@hp>
2015-10-11 07:04:01 -0400

    removed duplicate file

--- RedditAssholeDaily/src/CrawlRun.java
+++
@@ -1,9 +0,0 @@
-import java.util.Set;
-import reddit.*;
-
-public class CrawlRun {
-	public static void main(String[] args) {
-		new FrontpageCrawler().initiateCrawling();
-		Set<String> users = UserNameFetcher.getUserSet();
-	}
-}
