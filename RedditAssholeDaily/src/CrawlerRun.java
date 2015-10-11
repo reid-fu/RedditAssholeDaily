f0fd66ae4893828e22aec8713b5e0c3a29734da5
Viral Patel <Patel.2004@osu.edu>
2015-10-11 02:48:48 -0400

    stuff

--- RedditAssholeDaily/src/CrawlerRun.java
+++ RedditAssholeDaily/src/CrawlerRun.java
@@ -1,21 +1,31 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import judge.AlchemyJudge;
+import judge.CommentJudge;
+import reddit.FrontpageCrawler;
+import reddit.RedditParser;
+import reddit.UserNameFetcher;
+
 public class CrawlerRun {
 
     public static void main(String[] args) {
-        FrontPageCrawler fpc = new FrontPageCrawler();
+        FrontpageCrawler fpc = new FrontpageCrawler();
         fpc.initiateCrawling();
         Set<String> users = UserNameFetcher.getUserSet();
         Map<String, Double> map = new TreeMap<>();
-        
-        for(String str : users){
-            RedditParser rp = new RedditParser(str);
+
+        for (String str : users) {
+            RedditParser parser = new RedditParser(str);
             CommentJudge judge = new AlchemyJudge();
-            while((comment = parser.nextComment()) != null){
+            String comment;
+            while ((comment = parser.nextComment()) != null) {
                 judge.score(comment);
             }
             map.put(str, judge.cumulativeScore());
         }
-        
+
         System.out.println(map);
     }
-
 }
